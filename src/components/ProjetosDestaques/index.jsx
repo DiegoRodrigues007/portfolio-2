75ad61682a15893d6e06a3e2de2ba10e1491d3d4
Diego Rodrigues <diego.rodrigues3007@gmail.com>
2023-09-07 09:03:16 -0300

    modifiquei a parte do portfolio

--- src/components/ProjetosDestaques/index.jsx
+++ src/components/ProjetosDestaques/index.jsx
@@ -5,22 +5,22 @@ const ProjetosDestaques = () => {
   const conteudoProjeto = [
     {
       img: "/img/pc.png",
-      title: "Página Nike",
-      text: "ola",
+      title: "countdown-timer",
+      text: "Lorem Ipsum é simplesmente um texto fictício da indústria de impressão e composição tipográfica. Lorem Ipsum tem sido o texto fictício padrão da indústria desde 1500, quando um impressor desconhecido pegou uma prova de tipos e a misturou para fazer um livro de espécimes de tipos. Ela sobreviveu não apenas a cinco séculos, mas também ao salto para a composição tipográfica eletrônica, permanecendo essencialmente inalterada. Foi popularizado na década de 1960 com o lançamento de folhas Letraset contendo passagens de Lorem Ipsum e, mais recentemente, com software de editoração eletrônica como Aldus PageMaker, incluindo versões de Lorem Ipsum.",
+      site: "#",
+      git: "#",
+    },
+    {
+      img: "/img/pc.png",
+      title: "Bot Whatsapp",
+      text: "Lorem Ipsum é simplesmente um texto fictício da indústria de impressão e composição tipográfica.simplesmente um texto fictício da indústria de impressão e composição éculos, mas também ao salto para a composição tipográfica eletrônica, permanecendo essencialmente inalterada. Foi popularizado na década de 1960 com o lançamento de folhas Letraset",
       site: "#",
       git: "#",
     },
     {
       img: "/img/pc.png",
       title: "Página Nike",
-      text: "ola mundo",
-      site: "#",
-      git: "#",
-    },
-    {
-      img: "/img/pc.png",
-      title: "Página Nikeeeee",
-      text: "olaaaaaaaaaaaa",
+      text: "Lorem Ipsum é simplesmente um texto fictício da indústria de impressão e composição tipográfica.simplesmente um texto fictício da indústria de impressão e composição éculos, mas também ao salto para a composição tipográfica eletrônica, permanecendo essencialmente inalterada. Foi popularizado na década de 1960 com o lançamento de folhas Letraset",
       site: "#",
       git: "#",
     },
@@ -37,7 +37,7 @@ const ProjetosDestaques = () => {
       <div>
         {conteudoProjeto.map((conteudo) => (
           <div key={conteudo.key}>
-            <ProjetosImg img={conteudo.img} text={conteudo.text} title={conteudo.title} />
+            <ProjetosImg title={conteudo.title}  img={conteudo.img} text={conteudo.text}/>
           </div>
         ))}
       </div>
